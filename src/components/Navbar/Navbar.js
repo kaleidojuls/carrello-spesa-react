e46ae057bf1242f68011022ca2d35e817c81f8d6
Giulia s <g.schillaci@live.it>
2023-12-04 17:36:25 +0100

    brand-name ritorna ad home

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+import './Navbar.css';
+
 import Button from "../Button/Button";
 import Searchbar from "./Searchbar/Searchbar";
 
@@ -13,10 +15,16 @@ const Navbar = ({ setSearch, pageState }) => {
     const iconClass = page === 'home' ? "right" : "down";
     const counter = useSelector((state) => state.cartHandler.counter);
 
+    function returnHome() {
+        setPage('home');
+    }
+
     return (
         <nav className="navbar navbar-dark bg-dark fixed-top">
             <Searchbar setSearch={setSearch} pageState={pageState} />
-            <h1 className="navbar-brand">Shopping Online</h1>
+            <div className="title" onClick={() => returnHome()}>
+                <h1 className="navbar-brand">Shopping Online</h1>
+            </div>
             <Button className={`btn btn-${buttonClass}`} buttonOnClick={() => { setPage(newPage) }}>
                 <i className={`bi bi-caret-${iconClass}-fill`}></i> <i className="bi bi-cart4"></i>({counter})
             </Button>
